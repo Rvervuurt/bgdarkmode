d16423c7d1870925c0c543f46f8a0fc1aa7fc623
Robbert <robbert@webbook.dk>
2021-04-19 17:25:49 +0200

    Topic edit button fix

---
+++ js/common.js
@@ -0,0 +1,6 @@
+if(document.querySelector('[name="submit_topic"]')) {
+  document.getElementById("game-reactie-submit").setAttribute('value','Verzenden');
+
+  //Als we ooit de edit-pagina weten te raken, is dit de selector.
+  //document.getElementById("game-reactie-submit").parentElement.classList.add('topic_edit');
+}
